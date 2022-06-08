2b4ab044ed8db060618bf412cbc1c9e85521fbba
LocNguyen2510 <dinhloc2510@gmail.com>
2022-06-08 21:07:33 +0700

    Handle search, sort, filter on home page and handle login

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -3,16 +3,15 @@ import { Button } from "react-bootstrap";
 import './styles.scss'
 
 export default function Card({product}) {
-  console.log("PRODUCT:",product);
   return (
     <div className="card-product">
       {/* <div className="img"> */}
-        <img src={product.image} alt="" className="img" />
+        <img src={product.images[0]} alt="" className="img" />
       {/* </div> */}
       <div className="info-product">
         <h5>{product.name}</h5>
         <p>Hang:{product.brand}</p>
-        <p>{product.chip}</p>
+        <p>{product.cpu}</p>
         <p>Gia:{product.price}</p>
       </div>
       <div className="btn-view">
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,52 +1,172 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar";
-
+import axios from "axios";
 import Card from "../../components/card";
 import { data } from "../../data";
 import { Form, Button } from "react-bootstrap";
 import "./styles.scss";
 export default function Home() {
-  const [list, setList] = useState(data);
+  const [list, setList] = useState([]);
   const [search, setSearch] = useState("");
+  const [user, setUser] = useState([]);
   const [brand, setBrand] = useState("");
-  const [price, setPrice] = useState();
+
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    console.log("ham nay chay dau tien");
+    fetchAxios();
+  }, []);
+
+  const fetchAPI = () => {
+    fetch("https://reqres.in/api/users/", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const fetchAxios = () => {
+    axios
+      .get("https://lap-center.herokuapp.com/api/product")
+      .then(function (response) {
+        // handle success
+        console.log("SUCCESS:", response.data);
+        setList(response.data.products);
+      })
+      .catch(function (error) {
+        // handle error
+        console.error("ERROR:", error);
+      });
+  };
 
   const handChange = (val) => {
     console.log("value:", val);
     setSearch(val);
-    setList(
-      data.filter((item) =>
-        item?.name?.toLowerCase()?.includes(val.toLowerCase())
-      )
-    );
+    handleCallApi();
+    // setList(
+    //   data.filter((item) =>
+    //     item?.name?.toLowerCase()?.includes(val.toLowerCase())
+    //   )
+    // );
   };
+
   const onSubmitSearch = () => {
-    setList(
-      data.filter((item) =>
-        item?.name?.toLowerCase()?.includes(search.toLowerCase())
-      )
-    );
+    handleCallApi(search, brand, price);
+    // setList(
+    //   data.filter((item) =>
+    // //     item?.name?.toLowerCase()?.includes(search.toLowerCase())
+    // //   )
+    // // );
+    // axios
+    // .get("https://lap-center.herokuapp.com/api/product",{
+    //   params: {
+    //     productName: search,
+    //     productBrand: brand,
+    //     orderByColumn:'price',
+    //     orderByDirection: price
+
+    //   }
+    // })
+    // .then(function (response) {
+    //   // handle success
+    //   console.log("SUCCESS:", response.data);
+    //   setList(response.data.products)
+    // })
+    // .catch(function (error) {
+    //   // handle error
+    //   console.error("ERROR:", error);
+    // };
   };
+
   const handleSelectChange = (e) => {
     const val = e.target.value;
     setBrand(val);
-    setList(
-      data.filter((item) =>
-        item?.brand?.toLowerCase()?.includes(val.toLowerCase())
-      )
-    );
-    console.log(e.target.value);
+    handleCallApi(search, val, price);
+
+    // axios
+    // .get("https://lap-center.herokuapp.com/api/product",{
+    //   params: {
+    //     productName: search,
+    //     productBrand: val,
+    //     orderByColumn:'price',
+    //     orderByDirection: price
+
+    //   }
+    // })
+    // .then(function (response) {
+    //   // handle success
+    //   console.log("SUCCESS:", response.data);
+    //   setList(response.data.products)
+    // })
+    // .catch(function (error) {
+    //   // handle error
+    //   console.error("ERROR:", error);
+    // });
   };
+
   const sortPrice = (e) => {
+    console.log("check");
     const val = e.target.value;
     setPrice(val);
-    console.log(typeof val);
-    if (val === "1") {
-      setList(data.sort((a, b) => a.price - b.price));
-    } else {
-      setList(data.sort((a, b) => b.price - a.price));
-    }
+    handleCallApi(search, brand, val);
+
+    // console.log(typeof val);
+    // if (val === "1") {
+    //   setList(data.sort((a, b) => a.price - b.price));
+    // } else {
+    //   setList(data.sort((a, b) => b.price - a.price));
+    // }
+    // axios
+    // .get("https://lap-center.herokuapp.com/api/product",{
+    //   params: {
+    //     productName: search,
+    //     productBrand: brand,
+    //     orderByColumn:'price',
+    //     orderByDirection: val
+
+    //   }
+    // })
+    // .then(function (response) {
+    //   // handle success
+    //   console.log("SUCCESS:", response.data);
+    //   setList(response.data.products)
+    // })
+    // .catch(function (error) {
+    //   // handle error
+    //   console.error("ERROR:", error);
+    // });
   };
+  const handleCallApi = (productName, productBrand, productPrice) => {
+    axios
+      .get("https://lap-center.herokuapp.com/api/product", {
+        params: {
+          productName: productName,
+          productBrand: productBrand,
+          orderByColumn: "price",
+          orderByDirection: productPrice,
+        },
+      })
+      .then(function (response) {
+        // handle success
+        console.log("SUCCESS:", response.data);
+        setList(response.data.products);
+      })
+      .catch(function (error) {
+        // handle error
+        console.error("ERROR:", error);
+      });
+  };
+  // }
   return (
     <div className="homecontainer">
       <Navbar />
@@ -85,8 +205,8 @@ export default function Home() {
             <p>Giá</p>
             <select className="selectBox" value={price} onChange={sortPrice}>
               <option selected value=""></option>
-              <option value="1">Từ thấp đến cao</option>
-              <option value="2">Từ cao đến thấp</option>
+              <option value="asc">Từ thấp đến cao</option>
+              <option value="desc">Từ cao đến thấp</option>
             </select>
           </div>
         </div>
